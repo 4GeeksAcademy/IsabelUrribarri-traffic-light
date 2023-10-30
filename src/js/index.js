f9777a9b24b93b5349ddc25a298f3ed2d5317ce2
Isabel Urribarri <93502489+IsabelUrribarri@users.noreply.github.com>
2023-10-30 18:51:08 +0000

    terminado

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import Color from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Color />, document.querySelector("#app"));
